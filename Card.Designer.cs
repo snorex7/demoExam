163537388f5fd127886c105354560be1899ab3c3
snorex7 <hohulin007@gmail.com>
2026-04-29 11:17:08 +0400

    first commit

---
+++ Card.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace demoA
+{
+    partial class ProductCard
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductPicture = new System.Windows.Forms.PictureBox();
+            this.DiscountPanel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanelInfo = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelFabric = new System.Windows.Forms.Label();
+            this.labelSupplier = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelUnit = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.labelDiscount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductPicture)).BeginInit();
+            this.DiscountPanel.SuspendLayout();
+            this.tableLayoutPanelInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProductPicture
+            // 
+            this.ProductPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ProductPicture.Location = new System.Drawing.Point(6, 5);
+            this.ProductPicture.Name = "ProductPicture";
+            this.ProductPicture.Size = new System.Drawing.Size(165, 210);
+            this.ProductPicture.TabIndex = 0;
+            this.ProductPicture.TabStop = false;
+            // 
+            // DiscountPanel
+            // 
+            this.DiscountPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DiscountPanel.Controls.Add(this.labelDiscount);
+            this.DiscountPanel.Location = new System.Drawing.Point(430, 5);
+            this.DiscountPanel.Name = "DiscountPanel";
+            this.DiscountPanel.Size = new System.Drawing.Size(165, 210);
+            this.DiscountPanel.TabIndex = 1;
+            // 
+            // tableLayoutPanelInfo
+            // 
+            this.tableLayoutPanelInfo.ColumnCount = 2;
+            this.tableLayoutPanelInfo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelInfo.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelInfo.Controls.Add(this.labelCount, 1, 5);
+            this.tableLayoutPanelInfo.Controls.Add(this.labelUnit, 1, 4);
+            this.tableLayoutPanelInfo.Controls.Add(this.labelPrice, 1, 3);
+            this.tableLayoutPanelInfo.Controls.Add(this.labelSupplier, 1, 2);
+            this.tableLayoutPanelInfo.Controls.Add(this.labelFabric, 1, 1);
+            this.tableLayoutPanelInfo.Controls.Add(this.labelDescription, 1, 0);
+            this.tableLayoutPanelInfo.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanelInfo.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanelInfo.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanelInfo.Controls.Add(this.label6, 0, 4);
+            this.tableLayoutPanelInfo.Controls.Add(this.label7, 0, 5);
+            this.tableLayoutPanelInfo.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanelInfo.Location = new System.Drawing.Point(173, 40);
+            this.tableLayoutPanelInfo.Name = "tableLayoutPanelInfo";
+            this.tableLayoutPanelInfo.RowCount = 6;
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.19355F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.80645F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelInfo.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelInfo.Size = new System.Drawing.Size(255, 175);
+            this.tableLayoutPanelInfo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(186, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Категория товара | Наименование товара";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Описание товара:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Производитель:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Поставщик:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(3, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Цена:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(3, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Единица измерения:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(3, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Количество на складе:";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDescription.Location = new System.Drawing.Point(130, 0);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(64, 15);
+            this.labelDescription.TabIndex = 6;
+            this.labelDescription.Text = "Description";
+            // 
+            // labelFabric
+            // 
+            this.labelFabric.AutoSize = true;
+            this.labelFabric.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFabric.Location = new System.Drawing.Point(130, 69);
+            this.labelFabric.Name = "labelFabric";
+            this.labelFabric.Size = new System.Drawing.Size(37, 15);
+            this.labelFabric.TabIndex = 7;
+            this.labelFabric.Text = "Fabric";
+            // 
+            // labelSupplier
+            // 
+            this.labelSupplier.AutoSize = true;
+            this.labelSupplier.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSupplier.Location = new System.Drawing.Point(130, 93);
+            this.labelSupplier.Name = "labelSupplier";
+            this.labelSupplier.Size = new System.Drawing.Size(48, 15);
+            this.labelSupplier.TabIndex = 8;
+            this.labelSupplier.Text = "Supplier";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPrice.Location = new System.Drawing.Point(130, 114);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(31, 15);
+            this.labelPrice.TabIndex = 9;
+            this.labelPrice.Text = "Price";
+            // 
+            // labelUnit
+            // 
+            this.labelUnit.AutoSize = true;
+            this.labelUnit.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelUnit.Location = new System.Drawing.Point(130, 134);
+            this.labelUnit.Name = "labelUnit";
+            this.labelUnit.Size = new System.Drawing.Size(29, 15);
+            this.labelUnit.TabIndex = 10;
+            this.labelUnit.Text = "Unit";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCount.Location = new System.Drawing.Point(130, 154);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(37, 15);
+            this.labelCount.TabIndex = 11;
+            this.labelCount.Text = "Count";
+            // 
+            // labelDiscount
+            // 
+            this.labelDiscount.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDiscount.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDiscount.Location = new System.Drawing.Point(25, 77);
+            this.labelDiscount.Name = "labelDiscount";
+            this.labelDiscount.Size = new System.Drawing.Size(118, 115);
+            this.labelDiscount.TabIndex = 0;
+            this.labelDiscount.Text = "Действующая скидка";
+            // 
+            // ProductCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanelInfo);
+            this.Controls.Add(this.DiscountPanel);
+            this.Controls.Add(this.ProductPicture);
+            this.Name = "ProductCard";
+            this.Size = new System.Drawing.Size(601, 220);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductPicture)).EndInit();
+            this.DiscountPanel.ResumeLayout(false);
+            this.tableLayoutPanelInfo.ResumeLayout(false);
+            this.tableLayoutPanelInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ProductPicture;
+        private System.Windows.Forms.Panel DiscountPanel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelFabric;
+        private System.Windows.Forms.Label labelSupplier;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label labelUnit;
+        private System.Windows.Forms.Label labelDiscount;
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static demoA.Session;
+
+namespace demoA
+{
+    public partial class Products : Form
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void Products_Load(object sender, EventArgs e)
+        {
+            if (CurrentSession.CurrentUser != null)
+            {
+                if (CurrentSession.CurrentUser.Role == UserRole.Guest)
+                    labelUser.Text = $"Пользователь: Гость";
+                else
+                    labelUser.Text = $"Пользователь: {CurrentSession.CurrentUser.SurName} {CurrentSession.CurrentUser.FirstName} {CurrentSession.CurrentUser.MiddleName}";
+            }
+        }
+
+        private void btnExitUser_Click(object sender, EventArgs e)
+        {
+            CurrentSession.CurrentUser = null;
+
+            new Login().Show();
+
+            this.Close();
+        }
+    }
+}
